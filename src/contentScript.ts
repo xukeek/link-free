382a9bfc39e279396ddc4b671b94c0ec19d53229
xukeek <okeoke1011@gmail.com>
2020-09-12 22:01:49 +0800

    图标

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,10 +1,14 @@
-import $ from "jquery";
-
-const isNotFreeLink =
-    $("p.desc")
-        .text()
-        .trim() === "如需浏览，请长按网址复制后使用浏览器访问";
-if (isNotFreeLink) {
-    console.info("not");
-    window.location.href = $("p.link").text();
+const descList = document.querySelectorAll("p.desc");
+const linkList = document.querySelectorAll("p.link");
+if (
+    descList &&
+    linkList &&
+    descList.length > 0 &&
+    descList[0].textContent &&
+    descList[0].textContent.trim() ===
+        "如需浏览，请长按网址复制后使用浏览器访问" &&
+    linkList.length > 0 &&
+    linkList[0].textContent
+) {
+    window.location.href = linkList[0].textContent.trim();
 }
